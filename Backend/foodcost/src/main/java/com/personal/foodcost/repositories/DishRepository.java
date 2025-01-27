7e37aa02552d9d849a025c01f61f6efb1863c270
VocalicOyster <giovanni.innaimi@protonmail.com>
2025-01-27 13:50:05 +0100

    Added Repositories for all entities

---
+++ Backend/foodcost/src/main/java/com/personal/foodcost/repositories/DishRepository.java
@@ -0,0 +1,9 @@
+package com.personal.foodcost.repositories;
+
+import com.personal.foodcost.entities.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DishRepository extends JpaRepository<Dish, Long> {
+}
---
+++ Backend/foodcost/src/main/java/com/personal/foodcost/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.personal.foodcost.repositories;
+
+import com.personal.foodcost.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
